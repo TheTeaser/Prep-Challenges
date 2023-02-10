aa6d33f86f680396e7ae4b634f421df601437770
TheTeaser <theteaser98@gmail.com>
2023-02-10 12:00:24 +0000

    Updated as requested by TA to have a rule for negative

--- code-challenges/challenges-06.js
+++ code-challenges/challenges-06.js
@@ -37,7 +37,7 @@ const sumNums = (arr) => {
     let sum;
     sum = 0;
     for (let i = 0; i < arr.length; i++) {
-        if (typeof(arr[i]) == 'number') {
+        if (typeof(arr[i]) == 'number'&& arr[i]>=0) {
 
             sum = sum + arr[i];
         }
